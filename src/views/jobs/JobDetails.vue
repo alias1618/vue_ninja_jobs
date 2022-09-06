d70249a9f7447df3ecc3eca566bf3660c199d9e8
LAPTOP-VM1PS42M\Daniel <daniel.hong@essoft.com.tw>
2022-09-06 14:47:22 +0800

    20220906_Vue JS 3 Tutorial for Beginners #9 - Fetching Data___finish https://www.youtube.com/watch?v=7iDGJolHFmU&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=9

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>JobDetails Pagw</h1>
+    <!-- <h1>JobDetails Page</h1> -->
     <!-- 當 <p>The job id is {{ $route.params.id}}</p>   括號中間使用$route.params.id 時下方不需要 使用export default {data() {return {id: this.$route.params.id}} }
     -->
     <!-- <p>The job id is {{ $route.params.id}}</p> -->
@@ -8,7 +8,16 @@
         當 "<p>The job id is {{id}}</p> 使用id 下方需要
         expor default {data() {return{ id }}}
         "-->
-    <p>The job id is {{id}}</p>
+    <!-- <p>The job id is {{id}}</p> -->
+    
+    <div v-if="job">
+        <h1>{{ job.title}}</h1>
+        <p>The job id is {{id}}</p>
+        <P>{{ job.details}}</P>
+    </div>
+    <div v-else>
+        <p>Loading job details...</p>
+    </div>
 </template>
 
 <script>
@@ -19,6 +28,17 @@ export default {
     //         id: this.$route.params.id
     //     }
     // }
+    data(){
+        return {
+            job: null
+        }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs/' + this.id)
+            .then(res => res.json())
+            .then(data => this.job = data)
+            .catch(err => console.log(err.message))
+    },
 };
 </script>
 
