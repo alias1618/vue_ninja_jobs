e53169bbd1257a142fe35f2b84261fbc02202957
LAPTOP-VM1PS42M\Daniel <daniel.hong@essoft.com.tw>
2022-09-07 11:21:37 +0800

    20220907_Vue JS 3 Tutorial for Beginners #10 - The Composition API (part 1)_____finish https://www.youtube.com/watch?v=V-kxBWcPJfo&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=10

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -25,7 +25,7 @@ export default {
         }
     },   
     mounted() {
-        fetch('http://localhost:3000/jobs')
+        fetch('http://localhost:3000/jobs/')
             .then(res => res.json())
             .then(data => this.jobs = data)
             .catch(err => console.log(err.message))
